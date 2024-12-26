537ac2eba85894c6a10e75a56570f685bb3c92ee
Nitish-B2M <nitishssharma20@gnu.ac.in>
2024-12-26 06:14:15 +0530

    user request file

---
+++ users/pkg/payloads/request.go
@@ -0,0 +1,9 @@
+package payloads
+
+type UserUpdateRequest struct {
+	ID        int    `json:"id"`
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+	Email     string `json:"email"`
+	Gender    string `json:"gender"`
+}
